5369d772a23649b833e2792606fd0888a7fc8f31
tokmarkovka <tokmarkovka@gmail.com>
2022-10-25 23:53:10 +0300

    homework 02.09

---
+++ Homework1.py
@@ -0,0 +1,37 @@
+print('Задание 3\n')
+# Вывести в консоли
+
+a = 'import antigravity'
+b = 'import __hello__'
+c = 'from __future__ import braces'
+
+print(a, b, c, sep='\n')
+print('\n')
+
+print('Задание 4\n')
+# Вывести на консоль своё имя, нарисованное 'звёздочками'. Для выполнения этого задания надо
+# использовать не только символы звёздочки и пробела, но и ESCAPE символы: \n и \t
+
+my_awesome_name = ' ***   *   *  *****\t *****\t*\t\t *\t  *\t  *\t   *\t' \
+                  '\n*\t*  *   *  *\t  *\t   *\t*\t\t* *\t  *\t  *\t  *\t*\t' \
+                  '\n*\t   *   *  *\t\t   *\t*\t   *   *  **  *\t *\t *\t' \
+                  '\n ***   *   *  ***\t   *\t*\t   *   *  *\t* *\t *\t *\t' \
+                  '\n\t*  *   *  *\t\t   *\t*\t   *****  *\t **\t *****\t' \
+                  '\n*\t*\t* *\t  *\t  *\t   *\t*\t*  *   *  *\t  *\t *\t *\t' \
+                  '\n ***\t *\t  *****\t   *\t*****  *   *  *\t  *\t *\t *\t' \
+
+print(my_awesome_name)
+print('\n')
+
+print('Задание 5\n')
+# программа, которая выводит в консоль таблицу Escape-последовательностей:
+
+esc1 = '\\a   Bell (alert)'
+esc2 = '\\b   Backspace'
+esc3 = '\\n   New line'
+esc4 = '\\t   Horizontal tab'
+esc5 = '\\\\   Backslash \\'
+esc6 = '\\\"   Double quotation mark \"'
+esc7 = "\\\'   Single quotation mark \'"
+
+print(esc1, esc2, esc3, esc4, esc5, esc6, esc7, sep='\n')
